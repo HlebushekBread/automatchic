8852ceca1a1708da1ef47ef8d4b1c66522ed705e
Hlebushek <hlebushek.bread@gmail.com>
2026-03-15 12:45:24 +0300

    initial commit

---
+++ backend/src/test/java/net/breadlab/automatchic/AutomatchicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.breadlab.automatchic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatchicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
